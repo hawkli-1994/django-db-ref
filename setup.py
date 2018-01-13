1fb91118d68ad8cdac57cd07d37dffdd606c4362
hawkli <hawkli@yespowering.com>
2018-01-13 18:30:37 +0800

    upload pypi

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.8.2',  # replace "X.Y" as appropriate
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',  # example license
         'Operating System :: OS Independent',
